3a839e87d69cf28663c0d9be6562cceed83492d6
Cristian Avina <shaolinvswutang94@gmail.com>
2016-03-08 20:33:05 -0800

    Working on double red invariant for the add() function

--- Project4/src/RedBlackTree.java
+++ Project4/src/RedBlackTree.java
@@ -9,6 +9,7 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 		private K key;
 		private V value;
 		private Node left, right;
+		private Node parent;
 		private Color currentColor;
 
 		public Node(K key, V value) {
@@ -17,24 +18,56 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 			this.currentColor = Color.RED;
 			left = null;
 			right = null;
+			parent = null;
+		}
+
+		public boolean isRed() {
+			return currentColor == Color.RED;
+		}
+
+		public boolean isBlack() {
+			return !isRed();
 		}
 	}
-	
+
 	Node root = null;
+
 	public void add(K key, V value) {
 		// Case 1: Tree is empty
+		Node x = null; // local variable
 		if (root == null) {
 			root = new Node(key, value);
 			root.currentColor = Color.BLACK;
+			return;
 		} else {
-			BSTadd(key, value);
+			x = BSTadd(key, value);
+			treeFixup(x);
 		}
-
 		// end of method
 	}
 
+	// Helper function to fix tree
+	public void treeFixup(Node x) {
+		// Case: Parent is black
+		if (x.parent.isBlack()) {
+			return;
+		} else {
+			// Case: Double Red Invariant
+			while (x.parent.isRed()) {
+				if (x.parent == x.parent.parent.left) {
+					Node y = x.parent.parent.right;
+					if (y.isRed()) {
+						// uncle is red, re-color
+						reColoring(x, y);
+					}
+				}
+			}
+		}
+		//End of method
+	}
+
 	// Helper function to implement BST adding invariants
-	public void BSTadd(K addKey, V addValue) {
+	public Node BSTadd(K addKey, V addValue) {
 		Node current = root;
 		Node prev = null;
 		while (current != null) {
@@ -48,18 +81,31 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 
 		if (addKey.compareTo(prev.key) <= 0) {
 			prev.left = new Node(addKey, addValue);
+			prev.left.parent = prev;
+			return prev.left;
 		} else {
 			prev.right = new Node(addKey, addValue);
+			prev.right.parent = prev;
+			return prev.right;
 		}
 		// end of method
 	}
-	
-	public void printCase(){
+
+	public void rotateLeft() {
+
+	}
+
+	public void rotateRight(Node x, Node y) {
+
+	}
+
+	public void reColoring(Node x, Node y) {
+
+	}
+
+	public void printCase() {
 		System.out.println(root.key);
 		System.out.println(root.left.key);
-		System.out.println(root.right.key);
-		System.out.println(root.right.right.key);
-		System.out.println(root.right.left.key);
 	}
 
 	@Override
