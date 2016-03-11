224aa17428a56471a65c0e0fb30c828d7b7abd49
Cristian Avina Lopez <cavinalopez@cpp.edu>
2016-03-11 01:19:55 -0800

    Currently on right rotation... work in progress....

--- Project4/src/RedBlackTree.java
+++ Project4/src/RedBlackTree.java
@@ -39,16 +39,16 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 	}
 
 	public void add(K key, V value) {
-		// Case 1: Tree is empty
-		Node<K, V> x = null; // local variable
+		// Case: Tree is empty
+		Node<K, V> x = new Node<K, V>(key, value);
 		if (root == null) {
-			root = new Node<K, V>(key, value);
+			root = x;
 			root.currentColor = Color.BLACK;
 			addLeaves(root);
 			return;
 		} else {
 			x = BSTadd(key, value);
-			// treeFixup(x);
+			//treeFixup(x);
 		}
 		// end of method
 	}
@@ -61,16 +61,14 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 		} else {
 			while (x.parent.currentColor == Color.RED) {
 				if (x.parent == x.parent.parent.left) {
-					Node<K, V> y = x.parent.parent.right;
-					if(y.currentColor == Color.RED){
-						case1(x,y);
+					Node<K, V> uncle = x.parent.parent.right;
+					if(uncle.currentColor == Color.RED){
+						case1(x, uncle);
 					}else if(x == x.parent.right){
-						x = x.parent;
-						rotateLeft();
-						x.parent.currentColor = Color.BLACK;
-						x.parent.parent.currentColor = Color.RED;
-						rotateRight();
+						//case2(x, uncle);
 					}else{
+						//Uncle is black
+						case3(x); 
 						root.currentColor = Color.BLACK;
 					}
 				}
@@ -108,18 +106,48 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 
 	public void case1(Node<K, V> x, Node<K, V> y) {
 		// Case: Double Red Invariant
+		reColoring(x, y);
+		x = x.parent.parent; // Pointer is moved to grandparent
+	}
+
+	public void case2(Node<K, V> x, Node<K, V> uncle) {
+
+	}
+	
+	public void case3(Node<K, V> x){
+		x = x.parent;
+		//Implement Rotate Left
+		rotateLeft(x);
 		x.parent.currentColor = Color.BLACK;
-		y.currentColor = Color.BLACK;
 		x.parent.parent.currentColor = Color.RED;
-		x = x.parent.parent;
+		rotateRight();
 	}
-
-	public void case2(Node<K, V> x) {
-
-	}
-
-	public void rotateLeft() {
-
+	
+	/**
+	 * This left rotation is called when it hits the black uncle case when 
+	 * invariant 4 is broken, how this method works is it sets a pointer to the 
+	 * value being added (pointer y), if we are dealing with case N, it is assumed that there
+	 * is a pointer placed at the parent of pointer x. we then set the subtree connected to the left
+	 * of y to the right pointer of x. 
+	 * @param x
+	 * @param y
+	 */
+	public void rotateLeft(Node<K,V> x) {
+		Node<K, V> y = x.right;
+		x.right = y.left;
+		if (y.left != null){
+			y.left.parent = x;
+			y.parent = x.parent;
+		}
+		if(x.parent == null){
+			root = y;
+		}else if(x == x.parent.left){
+			x.parent.left = y;
+		}else{
+			x.parent.right = y;
+			y.left = x;
+			x.parent = y;
+		}
 	}
 
 	public void rotateRight() {
@@ -127,9 +155,9 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 	}
 
 	public void reColoring(Node<K, V> x, Node<K, V> y) {
-		x.parent.currentColor = Color.BLACK;
-		y.currentColor = Color.BLACK;
-		x.parent.parent.currentColor = Color.RED;
+		x.parent.currentColor = Color.BLACK; //added node's parent to black, restoring invariant 4
+		y.currentColor = Color.BLACK; //uncle is set to black to restore invariant 5
+		x.parent.parent.currentColor = Color.RED; //grandparent is set to RED
 
 	}
 
