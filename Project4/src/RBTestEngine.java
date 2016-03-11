066169a2aa1ae66818c03cddc93ab7dfa6e50297
Cristian Avina Lopez <cavinalopez@cpp.edu>
2016-03-10 17:44:48 -0800

    Commit....

--- Project4/src/RBTestEngine.java
+++ Project4/src/RBTestEngine.java
@@ -5,9 +5,6 @@ public class RBTestEngine {
 		RedBlackTree<Integer, String> newTree = new RedBlackTree<Integer, String>();
 		newTree.add(10, "Dog");
 		newTree.add(9, "Cat");
-		newTree.add(12, "Rat");
-		newTree.add(13, "Zebras");
-		newTree.add(11, "Mouse" );
 		newTree.printCase();
 	}
 }
--- Project4/src/RedBlackTree.java
+++ Project4/src/RedBlackTree.java
@@ -1,15 +1,15 @@
-
+//Cristian Avina Lopez
 public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 	public enum Color {
 		RED, BLACK
 	}
 
 	// Nested Node Class
-	private class Node {
+	private class Node<K, V> {
 		private K key;
 		private V value;
-		private Node left, right;
-		private Node parent;
+		private Node<K, V> left, right;
+		private Node<K, V> parent;
 		private Color currentColor;
 
 		public Node(K key, V value) {
@@ -30,47 +30,61 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 		}
 	}
 
-	Node root = null;
+	Node<K, V> root = null;
+
+	public void addLeaves(Node<K, V> x) {
+		x.left = new Node<K, V>(null, null);
+		x.left.currentColor = Color.BLACK;
+		x.right = new Node<K, V>(null, null);
+		x.right.currentColor = Color.BLACK;
+	}
 
 	public void add(K key, V value) {
 		// Case 1: Tree is empty
-		Node x = null; // local variable
+		Node<K, V> x = null; // local variable
 		if (root == null) {
-			root = new Node(key, value);
+			root = new Node<K, V>(key, value);
 			root.currentColor = Color.BLACK;
+			addLeaves(root);
 			return;
 		} else {
 			x = BSTadd(key, value);
-			treeFixup(x);
+			// treeFixup(x);
 		}
 		// end of method
 	}
 
 	// Helper function to fix tree
-	public void treeFixup(Node x) {
+	public void treeFixup(Node<K, V> x) {
 		// Case: Parent is black
 		if (x.parent.isBlack()) {
 			return;
 		} else {
-			// Case: Double Red Invariant
-			while (x.parent.isRed()) {
+			while (x.parent.currentColor == Color.RED) {
 				if (x.parent == x.parent.parent.left) {
-					Node y = x.parent.parent.right;
-					if (y.isRed()) {
-						// uncle is red, re-color
-						reColoring(x, y);
+					Node<K, V> y = x.parent.parent.right;
+					if(y.currentColor == Color.RED){
+						case1(x,y);
+					}else if(x == x.parent.right){
+						x = x.parent;
+						rotateLeft();
+						x.parent.currentColor = Color.BLACK;
+						x.parent.parent.currentColor = Color.RED;
+						rotateRight();
+					}else{
+						root.currentColor = Color.BLACK;
 					}
 				}
 			}
 		}
-		//End of method
+		// End of method
 	}
 
 	// Helper function to implement BST adding invariants
-	public Node BSTadd(K addKey, V addValue) {
-		Node current = root;
-		Node prev = null;
-		while (current != null) {
+	public Node<K, V> BSTadd(K addKey, V addValue) {
+		Node<K, V> current = root;
+		Node<K, V> prev = null;
+		while (current.key != null) {
 			prev = current;
 			if (addKey.compareTo(current.key) <= 0) {
 				current = current.left;
@@ -80,26 +94,43 @@ public class RedBlackTree<K extends Comparable<K>, V> implements Tree<K, V> {
 		}
 
 		if (addKey.compareTo(prev.key) <= 0) {
-			prev.left = new Node(addKey, addValue);
+			prev.left = new Node<K, V>(addKey, addValue);
+			addLeaves(prev.left);
 			prev.left.parent = prev;
 			return prev.left;
 		} else {
-			prev.right = new Node(addKey, addValue);
+			prev.right = new Node<K, V>(addKey, addValue);
+			addLeaves(prev.right);
 			prev.right.parent = prev;
 			return prev.right;
 		}
 		// end of method
 	}
 
+	public void case1(Node<K, V> x, Node<K, V> y) {
+		// Case: Double Red Invariant
+		x.parent.currentColor = Color.BLACK;
+		y.currentColor = Color.BLACK;
+		x.parent.parent.currentColor = Color.RED;
+		x = x.parent.parent;
+	}
+
+	public void case2(Node<K, V> x) {
+
+	}
+
 	public void rotateLeft() {
 
 	}
 
-	public void rotateRight(Node x, Node y) {
+	public void rotateRight() {
 
 	}
 
-	public void reColoring(Node x, Node y) {
+	public void reColoring(Node<K, V> x, Node<K, V> y) {
+		x.parent.currentColor = Color.BLACK;
+		y.currentColor = Color.BLACK;
+		x.parent.parent.currentColor = Color.RED;
 
 	}
 
